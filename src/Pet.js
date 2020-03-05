ae4457492fadeaff9b79997dbbea1c54abc85f9e
fleria kate <fleriakate@gmail.com>
2020-03-05 17:37:58 +0300

    4th March 2020 no2

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Pet = props => {
+const Pet = ({ name,animal,breed }) =>  {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
     </div>
   );
 };
