4689556e711cee7f2325aeb07f91d689ce1675a6
fleria kate <fleriakate@gmail.com>
2020-03-06 16:14:35 +0300

    6th March 2020 no3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from '@reach/router';
+import { Router,Link } from '@reach/router';
 import Details from './Details';
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Pets</h1>
+      <h1> className
+        <Link to="/">Adopt Pets</Link>
+      </h1>
       <Router>
       <SearchParams path="/" />
       <Details path="/details/:id" />
--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link } from "@reach/router";
 
 const Pet = props => {
   const { name, animal, breed, media, location, id } = props;
